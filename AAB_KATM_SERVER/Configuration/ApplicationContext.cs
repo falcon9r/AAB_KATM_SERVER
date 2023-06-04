f7e9f8381f364ac01979231b5fdb6ba73799c9d2
Mirzorasul <105106724+falcon9r@users.noreply.github.com>
2023-06-04 20:52:33 +0500

    Добавьте файлы проекта.

---
+++ AAB_KATM_SERVER/Configuration/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.EntityFrameworkCore;
+
+namespace AAB_KATM_SERVER.Configuration
+{
+    public class ApplicationContext : DbContext
+    {
+        public ApplicationContext() 
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Server = sql-server; Database = aab; User Id = SA; Password = RPSsql12345;TrustServerCertificate=true";
+            optionsBuilder.UseSqlServer(connectionString);
+            optionsBuilder.LogTo(Console.WriteLine);
+        }
+    }
+}
